4574a4236a5731fe5886ce123b826f30feaa54b2
swastikpoddar11 <swastikpoddar11@gmail.com>
2026-08-08 02:13:40 +0530

    Time: 0 ms (100%), Space: 44.3 MB (34.19%) - LeetHub

---
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> result = new ArrayList<>();
+        int current = 1;
+        int m = target.length;
+        for(int i =0; i<m;i++){
+            while(current<target[i]){
+                result.add("Push");
+                result.add("Pop");
+                current++;
+            }
+            result.add("Push");
+            current++;
+        }
+        return result;
+    }
+}
